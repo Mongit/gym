aa404b6a28fdeacfef008ea06c28138e4e671b68
jonasnahum <jonasnahum@gmail.com>
2016-06-21 11:29:54 -0500

    just for pull request

--- web/public/angular/src/editarController.js
+++ web/public/angular/src/editarController.js
@@ -3,59 +3,29 @@
     var depArr = ['$routeParams', '$location', 'proxyFactory' ];
     depArr.push(function($route, $location, proxyFactory ) {
         var ctrl = this;
+        ctrl.nombre = "";
+        ctrl.tipoPago = "";
+        ctrl.fechaInicio = "";
+        ctrl.fechaFin = "";
 
         ctrl.clienteId= $route.id;
 
         var getOne = function(id) {
             proxyFactory.getOne(id, function(data, status, headers, config){
-                ctrl.nombre = data.nombre;
-                ctrl.activo = data.activo,
-                ctrl.ultimoPago = data.ultimosPagos: [pagoSchema]
+                ctrl.nombre = data.nombre,
             });
         };
+
         getOne(ctrl.clienteId);
 
-        ctrl.quitarLogo = function() {
-            ctrl.logotipo = undefined;
-        };
-        ctrl.quitarFoto = function() {
-            ctrl.foto = undefined;
-        };
-
-        ctrl.borrarMarker = function () {
-            ctrl.mapa.borrarMarker();
-        };
-
-        ctrl.agregarProducto = function() {
-            ctrl.producto = prod.agregarProducto(ctrl.productos, ctrl.producto);
-        };
-        ctrl.removerProducto = function() {
-            ctrl.productos = prod.removerProducto(ctrl.productos, ctrl.remover);
-        };
-        ctrl.borrarProductos = function() {
-            ctrl.productos = prod.borrarProductos(ctrl.productos);
-        };
-
-
-        ctrl.agregarLink = function() {
-            ctrl.link = linkObj.agregarLink(ctrl.links, ctrl.link);
-        };
-        ctrl.removerLink = function() {
-            ctrl.links = linkObj.removerLink(ctrl.links, ctrl.numLinkARemover);
-        };
-        ctrl.borrarLinks = function() {
-            ctrl.links = linkObj.borrarLinks(ctrl.links);
-        };
 
         ctrl.editar = function(){
-            ctrl.lat = ctrl.mapa.getLat();
-            ctrl.long = ctrl.mapa.getLong();
-            paginasProxy.update(ctrl.empresaId, modelInstance.getModelFromCtrl(ctrl), function(data, status, headers, config){
-                alert("Info enviada");
-                $location.path('/privado/todos');
+            proxyFactory.update(ctrl.clienteId, ctrl, function(data, status, headers, config){
+                alert("Pago registrado");
+                $location.path('/');
             });
 
         };
     });
-    app.controller('EditarFormularioController', depArr);
+    app.controller('EditarController', depArr);
 })();
