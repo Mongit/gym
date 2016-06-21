2ed14be1093b609f0380e595c54b956c601bec91
Mongit <monserrat.jimenez2@gmail.com>
2016-06-21 11:52:03 -0500

    link en cada nombre que manda a la vista editar

--- web/public/angular/src/app.js
+++ web/public/angular/src/app.js
@@ -4,6 +4,7 @@
     app.config(["$routeProvider", function ($router) {
         $router.when("/", { templateUrl: "angular/views/todos.html" })
         $router.when("/nuevo", { templateUrl: "angular/views/nuevo.html" })
+        $router.when("/editar/:id", { templateUrl: "angular/views/editar.html" })
         .otherwise({ redirectTo: "/" });
     }]);
 })();
