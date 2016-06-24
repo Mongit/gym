24bccd3a5d647c7f564941a5d7c8089facb2d3f4
jonasnahum <jonasnahum@gmail.com>
2016-06-24 16:27:22 -0500

    url

--- web/public/angular/src/proxy.js
+++ web/public/angular/src/proxy.js
@@ -1,8 +1,8 @@
 (function() {
     var app = angular.module('app');
-    
+
     app.factory('proxy', ['proxyFactory', function(proxyFactory) {
-        var proxy = proxyFactory('http://localhost:3000/clientes/api/');
-        return proxy;     
+        var proxy = proxyFactory('clientes/api/');
+        return proxy;
     }]);
 })();
