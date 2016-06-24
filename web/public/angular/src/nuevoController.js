796d39d877e9ccfe20e8620752cc18e5bad8ad2f
Mongit <monserrat.jimenez2@gmail.com>
2016-06-24 14:32:39 -0500

    Merge with jonas pull

--- web/public/angular/src/nuevoController.js
+++ web/public/angular/src/nuevoController.js
@@ -19,40 +19,16 @@
 
         ctrl.getFechaFinBasadoEntipoPagoYfechaInicio = function(){
           var that = this;
-<<<<<<< HEAD
-          var sugerenciaInicioSting = that.getStringDateForDisplayFromMilliSec(ctrl.sugerenciaInicio);
-          ctrl.sugerenciaFin = fm.getFechaFin(sugerenciaInicioSting, ctrl.tipoPago);//fechaInicio es string. sugerenciafin retornara milisec.
-            ctrl.fechaFin = ctrl.sugerenciaFin;
-=======
           var sugerenciaInicioSting = that.getStringDateForDisplayFromMilliSec(Date.now());
           var fechaFinMillisec = fm.getFechaFin(sugerenciaInicioSting, ctrl.tipoPago);//fechaInicio es string. sugerenciafin retornara milisec.
           ctrl.fechaInicio = fm.getDateStringForDisplayInInput(Date.now());
           ctrl.fechaFin = fm.getDateStringForDisplayInInput(fechaFinMillisec);
->>>>>>> 69945a10e047b09ee9db5554d0129196eb11d9ab
         };
-var este = this;
+
         //Server Call
         ctrl.save = function() {
-<<<<<<< HEAD
-            //ctrl.fechaInicio = inicioModificada || ctrl.getStringDateForDisplayFromMilliSec(ctrl.sugerenciaInicio);
-            //ctrl.fechaFin = finModificada || ctrl.getStringDateForDisplayFromMilliSec(ctrl.sugerenciaFin);
-            
-            
-            
-            
-            
-            //ctrl.fechaInicio = ctrl.getStringDateForDisplayFromMilliSec(inicioModificada) || ctrl.getStringDateForDisplayFromMilliSec(ctrl.sugerenciaInicio);
-            ctrl.fechaInicio = inicioModificada || ctrl.getStringDateForDisplayFromMilliSec(ctrl.sugerenciaInicio);
-            
-          ctrl.sugerenciaFin = fm.getFechaFin(este.getStringDateForDisplayFromMilliSec(inicioModificada), ctrl.tipoPago);
-            //ctrl.fechaFin = ctrl.getFechaFinBasadoEntipoPagoYfechaInicio();
-            ctrl.fechaFin = ctrl.sugerenciaFin || ctrl.getStringDateForDisplayFromMilliSec(ctrl.fechaFin);
-            
-            
-=======
             ctrl.fechaInicio = ctrl.fechaInicioCambiada || ctrl.fechaInicio;
             ctrl.fechaFin = ctrl.fechaFinCambiada || ctrl.fechaFin;
->>>>>>> 69945a10e047b09ee9db5554d0129196eb11d9ab
             proxy.save(ctrl, function(data, status, headers, config){
                 $location.path('/');
             });
